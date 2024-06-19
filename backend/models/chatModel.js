a313ba75490e4950d0b5bb0cd9ab5b70d8160c8a
Pranjali Mishra <149889746+pranjali7117@users.noreply.github.com>
2024-06-19 21:44:57 +0530

    backend

---
+++ backend/models/chatModel.js
@@ -0,0 +1,64 @@
+//chatName
+//isgroupChat
+//users
+//latestMessage
+//groupAdmin
+
+// const mongoose = require('mongoose')
+
+// const chatModel = mongoose.Schema(
+//     {
+//         chatName: { type: String, trim: true },
+//         isGroupchat: { type: Boolean, default: false },
+//         users: [{
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//         },
+//         ],
+//         latestMessage: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Message",
+//         },
+//         groupAdmin: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//         },
+//     }
+//     {
+//         timeStamp: true,
+//     }
+// );
+
+// const chat = mongoose.model("Chat", chatModel);
+
+// module.exports = Chat;
+
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+    {
+        chatName: {
+             type: String, trim: true 
+            },
+        isGroupChat: {
+             type: Boolean, default: false 
+            },
+        users: [{
+             type: mongoose.Schema.Types.ObjectId, ref: "User" 
+            }],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        groupAdmin: { 
+            type: mongoose.Schema.Types.ObjectId, ref: "User" 
+        },
+    },
+    { 
+        timestamps: true 
+    }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
